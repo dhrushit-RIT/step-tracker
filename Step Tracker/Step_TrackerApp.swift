0e7dda9939efcfcd82d6a8591687b1b71c9c777d
dhrushit-RIT <70442122+dhrushit-RIT@users.noreply.github.com>
2024-08-05 08:26:56 -0700

    initial commit

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Dhrushit Suresh Raval on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
