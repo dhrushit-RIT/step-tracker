14f07706f670fee90ac7bdd5d62647274edb1984
dhrushit-RIT <70442122+dhrushit-RIT@users.noreply.github.com>
2024-08-06 16:53:15 -0700

    step bar chart inter-activity

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -29,6 +29,7 @@ struct DashboardView: View {
     
     @Environment(HealthkitManager.self) private var hkManager
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
+    @State private var rawSelectedDate: Date?
     @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
     
@@ -40,7 +41,13 @@ struct DashboardView: View {
         guard !hkManager.stepData.isEmpty else { return 0 }
         let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
         return totalSteps / Double(hkManager.stepData.count)
-        
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        return hkManager.stepData.first {
+            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
     }
     
     var body: some View {
@@ -71,21 +78,37 @@ struct DashboardView: View {
                         }
                         .foregroundStyle(.secondary)
                         .padding(.bottom, 12)
-
+                        
                         Chart {
+                            if let selectedHealthMetric {
+                                RuleMark(x: .value("Selected Metric", selectedHealthMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .annotation(
+                                        position: .top,
+                                        spacing: 0,
+                                        overflowResolution: .init(
+                                            x: .fit(to: .chart), y: .disabled
+                                        )) {
+                                            annotationView
+//                                            Text(selectedHealthMetric.value, format: .number)
+                                        }
+                            }
                             RuleMark(y: .value("Average", avgStepCount))
                                 .foregroundStyle(Color.secondary)
                                 .lineStyle(.init(lineWidth: 1, dash: [5, 10]))
-//                            ForEach(HealthMetric.mockData) { steps in
+                            // ForEach(HealthMetric.mockData) { steps in
                             ForEach(hkManager.stepData) { steps in
                                 BarMark(
                                     x: .value("Date", steps.date, unit: .day),
                                     y : .value("Steps", steps.value)
                                 )
                                 .foregroundStyle(.pink.gradient)
+                                .opacity(rawSelectedDate == nil || steps.date == selectedHealthMetric?.date ? 1.0 : 0.3)
                             }
                         }
                         .frame(height: 150)
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis {
                             AxisMarks {
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -141,6 +164,23 @@ struct DashboardView: View {
             })
         }
         .tint(isSteps ? .pink : .indigo)
+    }
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format: .dateTime.weekday().month(.abbreviated).day())
+                .font(.footnote.bold())
+                .foregroundStyle(.secondary)
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Color(.secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        )
     }
 }
 
