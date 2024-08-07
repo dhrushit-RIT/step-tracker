2047df84019d3021c55e0308af20e9faa70f96ef
dhrushit-RIT <70442122+dhrushit-RIT@users.noreply.github.com>
2024-08-07 12:45:24 -0700

    pie chart interactivity

--- Step Tracker/Charts/StepPieChart.swift
+++ Step Tracker/Charts/StepPieChart.swift
@@ -9,7 +9,21 @@ import SwiftUI
 import Charts
 
 struct StepPieChart: View {
+    
+    @State private var lastSelectedWeekdayInt: Int = 0
+    @State private var rawSelectedChartValue: Double? = 0
+
+    var selectedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return chartData[lastSelectedWeekdayInt - 1] }
+        var total = 0.0
+        let val = chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+        return val
+    }
     var chartData: [WeekdayChartData]
+
     var body: some View {
         VStack(alignment: .leading) {
             VStack(alignment: .leading) {
@@ -27,18 +41,46 @@ struct StepPieChart: View {
                     SectorMark(
                         angle: .value("Average Steps", weekday.value),
                         innerRadius: .ratio(0.618),
+                        outerRadius: selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
                         angularInset: 1
                     )
                     .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
+                    .opacity(selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1.0 : 0.3)
                 }
             }
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        let frame = geo[plotFrame]
+                        if let selectedWeekday {
+                            VStack {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                    .contentTransition(.identity)
+//                                    .animation(nil, value: UUID())
+                                    .transaction { transaction in
+                                        transaction.animation = nil
+                                    }
+                                Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: frame.midY)
+                        }
+                    }
+                }
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12)
             .fill(Color(.secondarySystemBackground)))
-
+        .onChange(of: rawSelectedChartValue) { oldValue, newValue in
+            lastSelectedWeekdayInt = selectedWeekday?.date.weekdayInt ?? 0
+        }
     }
 }
 
